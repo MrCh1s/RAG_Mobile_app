aeefa90b65dcba7a3179b6f024211259c3017047
Nguyễn Chí Đức <ducsieusao2005@gmail.com>
2026-04-28 21:38:06 +0700

    Temporarily bypass mach_task_basic_info for build debugging

--- ios_app/MLCChat/MLCChat/Common/Constants.swift
+++ ios_app/MLCChat/MLCChat/Common/Constants.swift
@@ -27,18 +27,8 @@ func debugLog(_ message: String, category: OSLog = .app) {
 
 struct MemoryUtil {
     static func getMemoryUsage() -> UInt64 {
-        var taskInfo = mach_task_basic_info()
-        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size) / 4
-        let kerr: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
-            $0.withMemoryRebound(to: integer_t.self, capacity: Int(count)) {
-                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
-            }
-        }
-        if kerr == KERN_SUCCESS {
-            return taskInfo.resident_size
-        } else {
-            return 0
-        }
+        // Temporary bypass to fix build failing due to Mach API visibility
+        return 1024 * 1024 * 500 // 500 MB dummy for testing build
     }
 
     static func getMemoryUsageString() -> String {
